c83661cadbb7be9b43266d10354630b678d70c69
G9D4 <gldt2494@gmail.com>
2021-10-28 09:25:39 -0500

    funcion de perfiles

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import data from './data/rickandmorty/rickandmorty.js';
-
-
-console.log(data);
+    fetch('./data/rickandmorty/rickandmorty.json')
+    .then(function(info){ //primer then establece la coneccion con el archivo
+        return info.json();
+    })
+    .then(function(data){ //segundo then me llama la data de .json
+    let person = data.results;
+    let list=''
+    person.forEach(function(element){
+        list+= `<div class="card">
+        <div>
+        <img src=${element.image}>
+        </img><h1>${element.name}</h1>
+        </div>
+        <div class="info">
+        <ul>
+        <li>Estado: ${element.status}</li>
+        <li>Especie: ${element.species}</li>
+        <li>Genero: ${element.gender}</li>
+        <li>Origen: ${element.origin.name}</li>
+        <li>Locacion: ${element.location.name}</li>
+        </ul>
+        </div>
+        </div>`
+    })
+    document.getElementById("profiles").innerHTML=list
+    })
