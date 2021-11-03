2246b4e2480c39bfa97e83401f40d86c47dd96e6
G9D4 <gldt2494@gmail.com>
2021-11-02 23:17:14 -0500

    funciones y css

--- src/main.js
+++ src/main.js
@@ -3,30 +3,11 @@ fetch('./data/rickandmorty/rickandmorty.json')
         return info.json();
     })
     .then(function(data){ //segundo then me llama la data de .json
-    let person = data.results;
-    let list=''//declaramos una variable que recopila el perfil de cada personaje
-    person.forEach(function(element){
-        list+= `<div class="card">
-        <div>
-        <img src=${element.image}>
-        </img><h1>${element.name}</h1>
-        </div>
-        <div class="info">
-        <ul>
-        <li>Estado: ${element.status}</li>
-        <li>Especie: ${element.species}</li>
-        <li>Genero: ${element.gender}</li>
-        <li>Origen: ${element.origin.name}</li>
-        <li>Locacion: ${element.location.name}</li>
-        </ul>
-        </div>
-        </div>`        
-    })
-   document.getElementById("profiles").innerHTML=list
-  
+    let person = data.results;  
    
+let getResults=document.getElementById("applyFilters")
+getResults.addEventListener('click',() =>{
 let filterGender = document.getElementById("gender");//filtrar por genero
-filterGender.addEventListener('change', () => {//usamos change para que el filtro sea inmediato cuando se cambia la opcion del select
 let genderChoice=filterGender.value
 let genderResults=person.filter((gen)=>{
     if(gen.gender===genderChoice){//comparamos el genero del personaje con el valor de genero elegido
@@ -37,13 +18,9 @@ let genderResults=person.filter((gen)=>{
     }
 })
 console.log(genderResults)
-})
-
-
 let filterSpecies = document.getElementById("species");//filtrar por especie
-filterSpecies.addEventListener('change', () => {
 let speciesChoice=filterSpecies.value
-let speciesResults=person.filter((spe)=>{
+let speciesResults=genderResults.filter((spe)=>{
     if(spe.species==speciesChoice){
         return true
     }else{
@@ -52,13 +29,9 @@ let speciesResults=person.filter((spe)=>{
     }
 })
 console.log(speciesResults)
-})
-
-
 let orderAlpha=document.getElementById("alphabet")//ordenar por orden alfabetico
-orderAlpha.addEventListener('change', () =>{
     let alphaChoice=orderAlpha.value
-    let alphaResults=person.sort((a,b)=>{//funcion sort
+    let alphaResults=speciesResults.sort((a,b)=>{//funcion sort
         if(alphaChoice==="ascend"){//usamos ifs para saber si ordenamos de forma ascendente o descendente
                 if(a.name<b.name){
                     return -1
@@ -85,10 +58,29 @@ orderAlpha.addEventListener('change', () =>{
             }
     })  
     console.log(alphaResults)
+    let list=''//declaramos una variable que recopila el perfil de cada personaje
+    alphaResults.forEach(function(element){
+        list+= `<div class="card">
+        <div class="front">
+        <img src=${element.image}></img>
+        <h2>${element.name}</h2>
+        </div>
+        <div class="info">
+        <ul>
+        <li>Estado: ${element.status}</li>
+        <li>Especie: ${element.species}</li>
+        <li>Genero: ${element.gender}</li>
+        <li>Origen: ${element.origin.name}</li>
+        <li>Locacion: ${element.location.name}</li>
+        </ul>
+        </div>
+        </div>`        
+    })
+   document.getElementById("profiles").innerHTML=list
     })
 })
 
-//Ya se puede buscar y ordenar pero como proceso independiente, faltaria poder hacer busquedas cruzadas.
-//Para eso deberiamos poner las funciones de filtrar y ordenar en data.js
-//Ahi le dariamos inicio a todo con el boton de Aplicar filtros y orden
-//La data de person iria pasando por cada filtro, reduciendo la info hasta obtener la info completamente filtrada y ordenada
+//Poner las funciones de filtrar y ordenar en data.js
+//Pensar como hacer busqueda multiple
+//Ordenar cards
+//Hacer otros HTML
